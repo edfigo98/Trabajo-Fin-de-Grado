bf1b03e0ceff2e41b3b9ba871ff8db84a0f8dbd6
edfigo98 <alu0101043838@ull.edu.es>
2021-07-21 19:35:12 +0100

    front

--- src/frontend/src/components/RequestData.vue
+++ src/frontend/src/components/RequestData.vue
@@ -26,7 +26,7 @@
         <v-select
           v-model="hora"
           :items="horas"
-          label="Intervalo previsto de llegada"
+          label="Hora prevista de llegada"
         />
       </v-col>
     </v-row>
@@ -41,17 +41,18 @@
               Destino
             </th>
             <th class="text-left">
-              Intervalo de salida
+              Hora de salida
             </th>
           </tr>
         </thead>
         <tbody>
           <tr
-            v-for="item in desserts"
-            :key="item.name"
+            v-for="item in intervalo"
+            :key="item"
           >
-            <td>{{ item.name }}</td>
-            <td>{{ item.calories }}</td>
+            <td>{{ item.salida }}</td>
+            <td>{{ item.destino }}</td>
+            <td>{{ item.hora }}</td>
           </tr>
         </tbody>
       </template>
@@ -72,7 +73,10 @@
         hora: '',
         lugares_salida: ['ID 1: Los Realejos', 'ID 3: Higuerita - Los Realejos', 'ID 12: Alcampo La Villa', 'ID 17: Polígono San Nicolás', 'ID 26: El Ramal - La Orotava', 'ID 36: La Cuesta de la Villa', 'ID 43: La Quinta - Santa Úrsula', 'ID 47: Alteza - Santa Úrsula', 'ID 52: La Victoria', 'ID 59: La Matanza', 'ID 70: El Sauzal', 'ID 76: Tacoronte', 'ID 82: Los Naranjeros - Tacoronte', 'ID 92: Guamasa - La Laguna', 'ID 96: Aeropuerto Tenerife Norte', 'ID 108: Padre Anchieta - La Laguna', 'ID 113: Guajara'],
         lugares_destino: ['ID 96: Aeropuerto Tenerife Norte', 'ID 108: Padre Anchieta - La Laguna', 'ID 113: Guajara', 'ID: 120: Camino La Hornera - Santa Cruz de Tenerife'],
-        horas: ['6:00 - 6:15', '6:15 - 6:30', '6:30 - 6:45', '6:45 - 7:00', '7:00 - 7:15', '7:15 - 7:30', '7:30 - 7:45', '7:45 - 8:00', '8:00 - 8:15', '8:15 - 8:30', '8:30 - 8:45', '8:45 - 9:00',]
+        horas: ['6:00 - 6:15', '6:15 - 6:30', '6:30 - 6:45', '6:45 - 7:00', '7:00 - 7:15', '7:15 - 7:30', '7:30 - 7:45', '7:45 - 8:00', '8:00 - 8:15', '8:15 - 8:30', '8:30 - 8:45', '8:45 - 9:00',],
+        origen: [],
+        destino_: [],
+        intervalo: [],
       }
     },
 
@@ -82,10 +86,15 @@
         connection.onopen = () => {
           // insert data as it arrives from the socket
           connection.send(`{ "salida": "${this.salida}", "destino": "${this.destino}", "hora": "${this.hora}" }`)          
-          // alert('Obteniendo datos desde...')
           connection.onmessage = async (event) => {
             alert(event.data)
-            // const dat = JSON.parse(event.data)
+            var jsonObject = {
+              "salida": this.salida,
+              "destino": this.destino,
+              "hora": event.data
+            }
+            this.intervalo.push(jsonObject)
+            console.log(this.intervalo)
           }
         }
       },
@@ -99,16 +108,6 @@
 .button-request {
   margin-top: 60px;
   margin-left: 60px;
-}
-
-.includemin {
-  margin-left: 120px;
-  margin-top: -50px;
-}
-
-.includemax {
-  margin-left: 120px;
-  margin-top: -50px;
 }
 
 #update {
